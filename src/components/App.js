6fad8ad8b2e4cb7258a013a348079319d4821b71
Jacopo Ghisolfi <jacopo.ghisolfi@devolute.org>
2018-02-01 13:01:33 +0100

    load more movies with a button

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../App.css';
 import Header from './Header';
 import Movie from './Movie';
 import {initialMovies} from '../movies';
+import {additionalMovies} from '../movies';
 
 class App extends Component {
 
@@ -12,8 +13,16 @@ class App extends Component {
     this.state = {
       movies: initialMovies
     };
+
+    this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this);
   }
 
+  loadAdditionalMovies() {
+    var currentMovies = { ...this.state.movies };
+    var newMovies = Object.assign( currentMovies, additionalMovies );
+
+    this.setState({movies: newMovies});
+  }
 
   render() {
     return (
@@ -28,6 +37,9 @@ class App extends Component {
               .keys(this.state.movies)
               .map(key => <Movie key={key} meta={this.state.movies[key]} />)
           }
+        </div>
+        <div className="add-movies">
+          <button onClick={this.loadAdditionalMovies}>Load more...</button>
         </div>
       </div>
     );
--- src/movies.js
+++ src/movies.js
@@ -26,4 +26,32 @@ const initialMovies = {
   }
 };
 
+const additionalMovies = {
+  movie6: {
+    title: "Il Buono, il Brutto e il Cattivo",
+    year: "1966",
+    description: "Spaghetti Western",
+    poster: "./posters/jaws.jpg"
+  },
+  movie7: {
+    title: "Jaws",
+    year: "1975",
+    description: "Mystery/Thriller",
+    poster: "./posters/il-buono-il-brutto-il-cattivo.jpg"
+  },
+  movie8: {
+    title: "Back To The Future",
+    year: "1985",
+    description: "",
+    poster: "./posters/full-metal-jacket.jpg"
+  },
+  movie9: {
+    title: "Full Metal Jacket",
+    year: "1987",
+    description: "War Drama",
+    poster: "./posters/back-to-the-future.jpg"
+  }
+};
+
 export {initialMovies};
+export {additionalMovies};
